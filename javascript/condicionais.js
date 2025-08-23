5652e449e3c7ced5554c8ba61c6e9bbcecac1563
RaffaFeitosa <rafael.feitosa91.silva@gmail.com>
2025-08-22 21:51:29 -0300

    Inicio de JavaScript

---
+++ javascript/condicionais.js
@@ -0,0 +1,12 @@
+// Exercicio 1
+let idade = prompt("Qual é a idade")
+
+if (idade >= 18) {
+
+alert("Parabens! Voce é maior de idade.")
+
+} else {
+
+    alert("Vai pra Super Nani!")
+
+}
---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+//console.log("Hello World!");
+
+// const valorFixo = "Rafael"
+
+// let nome = prompt("Qual seu nome?");
+// let idade = prompt("Qual a sua idade?")
+
+// alert("Meu nome é " + nome + " tenho " + idade + " anos");
+
+
