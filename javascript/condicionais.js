27eb3e5530a7f8529a78533944a28e77e1b5daea
RaffaFeitosa <rafael.feitosa91.silva@gmail.com>
2025-08-25 21:52:51 -0300

    Exercicios de JavaScript

--- javascript/condicionais.js
+++ javascript/condicionais.js
@@ -1,12 +1,120 @@
 // Exercicio 1
-let idade = prompt("Qual é a idade")
+// let idade = prompt("Qual é a idade");
 
-if (idade >= 18) {
+// if (idade >= 18) {
 
-alert("Parabens! Voce é maior de idade.")
+// alert("Parabens! Voce é maior de idade.");
 
-} else {
+// } 
+// else {
 
-    alert("Vai pra Super Nani!")
+//     alert("Vai pra Super Nani!");
 
-}
+// }
+
+//Exercicio 2
+// let numero = prompt("Digite um numero com sinal")
+
+// if (numero > 0) {
+
+//     alert("Seu numero é positvo")
+
+
+// } else {
+
+//     if (numero == 0) {
+//         alert("Neutro")
+
+//     } else{
+//         alert("Seu numero é negativo")
+
+//     }
+
+// }
+
+//Exercicio 3
+// let nota = prompt("Qual é a sua nota?");
+
+// if (nota >= 60) {
+
+//     alert("APROVADO");
+
+// }
+// else {
+
+//     alert("REPROVADO");
+
+// }
+
+//Exercicio 4
+// let numero = prompt("Digite um numero com sinal")
+
+// if (numero > 0) {
+
+//     alert("Seu numero é positvo")
+
+
+// } else {
+
+//     if (numero == 0) {
+//         alert("Neutro")
+
+//     } else {
+//         alert("Seu numero é negativo")
+
+//     }
+// }
+
+//Exercicio 5
+// let idade = prompt("5 Insira sua idade:");
+
+// if (idade > 0 && idade <= 12) {
+
+//     alert("Criança");
+
+// } else {
+
+//     if (idade > 12 && idade <= 17) {
+
+//         alert("Adolescente");
+
+//     }else 
+//         alert("Adulto")
+// }
+
+//Exercicio 6
+// let numero =prompt("Digite um numero");
+
+//     if (numero %2 == 0) {
+
+//         alert("O numero é par")
+//     }else{
+
+//         alert("O numero é impar")
+//     } 
+
+// INTERMEDIARIO
+// Exercicio 1
+let numero1 = Number (prompt("insira um numero"))
+let numero2 = Number (prompt("insira um numero"))
+let operacao =prompt("insira a operação")
+
+    if (operacao == "+"){
+        alert(numero1 + numero2)
+    }else {
+        if (operacao == "-"){
+            alert(numero1 - numero2)
+        } else{
+            if (operacao == "*"){
+                alert(numero1 * numero2)
+            }else{
+                if (operacao == "/")
+                    alert(numero1 / numero2)
+            }
+        }
+
+    
+
+    }
+
+   
